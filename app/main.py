d3874a66b62c89a564d0a69bb313fc280145b5be
HoangVietPW26 <nhviet26@gmail.com>
2025-01-24 23:34:29 +0000

    response with header and question and 1 answer

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import socket
+from models.DNSResponse import DNSResponse, DNSMessegeQuestion, DNSMessegeAnswer, DNSMessegeHeader
+
 
 
 def main():
@@ -14,28 +16,35 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
 
-            header = (b"\x04\xd2" +  # ID: 1234
-                      b"\x80" +      # QR=1, OPCODE=0, etc.
-                      b"\x00" +      # More flags
-                      b"\x00\x01" +  # QDCOUNT: 1 question
-                      b"\x00\x01" +  # ANCOUNT: 1 answer
-                      b"\x00\x00" +  # NSCOUNT: 0
-                      b"\x00\x00")   # ARCOUNT: 0
+            # header = (b"\x04\xd2" +  # ID: 1234
+            #           b"\x80" +      # QR=1, OPCODE=0, etc.
+            #           b"\x00" +      # More flags
+            #           b"\x00\x01" +  # QDCOUNT: 1 question
+            #           b"\x00\x01" +  # ANCOUNT: 1 answer
+            #           b"\x00\x00" +  # NSCOUNT: 0
+            #           b"\x00\x00")   # ARCOUNT: 0
+            
+            header = DNSMessegeHeader(1234, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0).get_header()
 
             # Question Section
-            question = (b"\x0ccodecrafters\x02io\x00" +   # Name 
-                    b"\x00\x01" +  # Type: A record 
-                    b"\x00\x01")    # Class: IN
+            # question = (b"\x0ccodecrafters\x02io\x00" +   # Name 
+            #         b"\x00\x01" +  # Type: A record 
+            #         b"\x00\x01")    # Class: IN
+
+            question = DNSMessegeQuestion("codecrafters.io", 1, 1).get_question()
+
 
             # Answer Section
-            answer = (b"\x0ccodecrafters\x02io\x00" +  # Name
-                    b"\x00\x01" +  # Type: A record
-                    b"\x00\x01" +  # Class: IN
-                    b"\x00\x00\x00\x3c" +  # TTL: 60
-                    b"\x00\x04" +  # Length: 4 bytes
-                    b"\x08\x08\x08\x08")  # IP: 8.8.8.8
+            # answer = (b"\x0ccodecrafters\x02io\x00" +  # Name
+            #         b"\x00\x01" +  # Type: A record
+            #         b"\x00\x01" +  # Class: IN
+            #         b"\x00\x00\x00\x3c" +  # TTL: 60
+            #         b"\x00\x04" +  # Length: 4 bytes
+            #         b"\x08\x08\x08\x08")  # IP: 8.8.8.8
+
+            answer = DNSMessegeAnswer("codecrafters.io", 1, 1, 60, 4, "8.8.8.8").get_answer()
             
-            response = header + question + answer
+            response = DNSResponse(header, question, answer).get_response()
     
             udp_socket.sendto(response, source)
         except Exception as e:
---
+++ app/models/DNSResponse.py
@@ -0,0 +1,62 @@
+class DNSMessegeHeader():
+    def __init__(self, ID=0, QR=0, OPCODE=0, AA=0, TC=0, RD=0, RA=0, Z=0, RCODE=0, QDCOUNT=1, ANCOUNT=0, NSCOUNT=0, ARCOUNT=0):
+        self.id = ID.to_bytes(2, 'big')
+        self.qr = QR.to_bytes(1, 'big')
+        self.opcode = OPCODE.to_bytes(1, 'big')
+        self.aa = AA.to_bytes(1, 'big')
+        self.tc = TC.to_bytes(1, 'big')
+        self.rd = RD.to_bytes(1, 'big')
+        self.ra = RA.to_bytes(1, 'big')
+        self.z = Z.to_bytes(1, 'big')
+        self.rcode = RCODE.to_bytes(1, 'big')
+        self.qdcount = QDCOUNT.to_bytes(2, 'big')
+        self.ancount = ANCOUNT.to_bytes(2, 'big')
+        self.nscount = NSCOUNT.to_bytes(2, 'big')
+        self.arcount = ARCOUNT.to_bytes(2, 'big')
+    
+    def get_header(self):
+        return self.id + self.qr + self.opcode + self.aa + self.tc + self.rd + self.ra + self.z + self.rcode + self.qdcount + self.ancount + self.nscount + self.arcount
+
+class DNSMessegeQuestion():
+    def __init__(self, NAME="codecrafters.io", QTYPE=1, QCLASS=1):
+        self.name = self.get_name(NAME)
+        self.qtype = QTYPE.to_bytes(2, 'big')
+        self.qclass = QCLASS.to_bytes(2, 'big')
+    
+    def get_name(self):
+        names = self.name.split(b'.')
+        for i in range(len(names)):
+            names[i] = len(names[i]).to_bytes(1, 'big') + names[i]
+        self.name = b''.join(names) + b'\x00'
+        return self.name
+    
+    def get_question(self):
+        return self.name + self.qtype + self.qclass
+
+class DNSMessegeAnswer():
+    def __init__(self, NAME="codecrafters.io", TYPE=1, CLASS=1, TTL=60, RDLENGTH=4, RDATA="8.8.8.8"):
+        self.name = self.get_name(NAME)
+        self.type = TYPE.to_bytes(2, 'big')
+        self.class_ = CLASS.to_bytes(2, 'big')
+        self.ttl = TTL.to_bytes(4, 'big')
+        self.rdlength = RDLENGTH.to_bytes(2, 'big')
+        self.rdata = RDATA.to_bytes(4, 'big')
+    
+    def get_name(self):
+        names = self.name.split(b'.')
+        for i in range(len(names)):
+            names[i] = len(names[i]).to_bytes(1, 'big') + names[i]
+        self.name = b''.join(names) + b'\x00'
+        return self.name
+    
+    def get_answer(self):
+        return self.name + self.type + self.class_ + self.ttl + self.rdlength + self.rdata
+
+class DNSResponse():
+    def __init__(self, header, question, answer):
+        self.header = header
+        self.question = question
+        self.answer = answer
+    
+    def get_response(self):
+        return self.header + self.question + self.answer
