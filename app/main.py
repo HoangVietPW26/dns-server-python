6a40f6110c5bb08eb9d8b88e94adbfad1d729f09
HoangVietPW26 <nhviet26@gmail.com>
2025-01-24 22:54:09 +0000

    response with header and question

--- app/main.py
+++ app/main.py
@@ -13,8 +13,11 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-    
-            response = b"\x04\xd2\x80" + (b"\x00")*9
+
+            header = b"\x04\xd2\x80\x00\x00\x01" + (b"\x00")*6
+            question = b"\x0c\x63\x6F\x64\x65\x63\x72\x61\x66\x74\x65\x72\x73" + \
+                b"\x02\x69\x6F" + b"\x00\x01" + b"\x00\x01" + b"\x00"
+            response = header + question
     
             udp_socket.sendto(response, source)
         except Exception as e:
