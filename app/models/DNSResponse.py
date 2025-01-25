e09ccb2705e5219346cdabea2e3b3d7a2cbe1f40
HoangVietPW26 <nhviet26@gmail.com>
2025-01-25 02:43:10 +0000

    dynamic response with header and question & answer

--- app/models/DNSResponse.py
+++ app/models/DNSResponse.py
@@ -73,6 +73,7 @@ def decode_dns_question(question, num_of_question=1, start=12):
         names.append(('.'.join(name), qtype, qclass))
         name = []
         i += 5
+        print(question[i])
         num_of_question -= 1
     return names
 
