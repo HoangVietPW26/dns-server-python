4cea6b56ae0624e9464633c7024b37de132f19ec
HoangVietPW26 <nhviet26@gmail.com>
2025-01-25 02:41:44 +0000

    dynamic response with header and question & answer

--- app/models/DNSResponse.py
+++ app/models/DNSResponse.py
@@ -58,6 +58,7 @@ def decode_dns_question(question, num_of_question=1, start=12):
     while num_of_question > 0:
         while question[i] != 0:
             if question[i] >= 192:
+                print("jjjj")
                 offset = int.from_bytes(question[i:i+2], 'big') - 49152
                 name.append(decode_compress(question, offset))
                 i += 2
