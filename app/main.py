6affbf4672d110af061c3170464e55c2b0158daf
HoangVietPW26 <nhviet26@gmail.com>
2025-01-25 01:08:14 +0000

    dynamic response with header and question & answer

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ def main():
             question = DNSMessegeQuestion(name, 1, 1).get_question()
             print(question)
 
-            # print(buf[start:])
+            print(buf[12:])
             (name, _type, _class, _ttl, _rdlength, _rdata) = decode_dns_answer(buf[12:], start)
             answer = DNSMessegeAnswer(name, 1, 1, 60, 4, "8.8.8.8").get_answer()
             print(answer)
